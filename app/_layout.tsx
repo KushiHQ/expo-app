30a4845fbefe029c83760b374ab9b1602fc2b4b7
Ezekiel Victor <codeepoch@gmail.com>
2025-10-08 18:39:49 +0100

    Feat: initial commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,66 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import {
+	DarkTheme,
+	DefaultTheme,
+	ThemeProvider,
+} from "@react-navigation/native";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
+import * as SplashScreen from "expo-splash-screen";
+import {
+	Inter_100Thin,
+	Inter_200ExtraLight,
+	Inter_300Light,
+	Inter_400Regular,
+	Inter_500Medium,
+	Inter_600SemiBold,
+	Inter_700Bold,
+	Inter_800ExtraBold,
+	Inter_900Black,
+	useFonts,
+} from "@expo-google-fonts/inter";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { useColorScheme } from "@/lib/hooks/use-color-scheme";
+import { Fonts } from "@/constants/theme";
+import React from "react";
+import "../global.css";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+	const colorScheme = useColorScheme();
+	const [loaded, error] = useFonts({
+		[Fonts.thin]: Inter_100Thin,
+		[Fonts.extralight]: Inter_200ExtraLight,
+		[Fonts.light]: Inter_300Light,
+		[Fonts.regular]: Inter_400Regular,
+		[Fonts.medium]: Inter_500Medium,
+		[Fonts.semibold]: Inter_600SemiBold,
+		[Fonts.bold]: Inter_700Bold,
+		[Fonts.extrabold]: Inter_800ExtraBold,
+		[Fonts.black]: Inter_900Black,
+	});
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+	React.useEffect(() => {
+		if (loaded || error) {
+			SplashScreen.hideAsync();
+		}
+	}, [loaded, error]);
+
+	if (!loaded && !error) {
+		return null;
+	}
+
+	return (
+		<SafeAreaProvider>
+			<ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+				<Stack>
+					<Stack.Screen name="index" options={{ headerShown: false }} />
+					<Stack.Screen name="onboarding" options={{ headerShown: false }} />
+				</Stack>
+				<StatusBar style="auto" />
+			</ThemeProvider>
+		</SafeAreaProvider>
+	);
 }
--- constants/theme.ts
+++ constants/theme.ts
@@ -3,51 +3,47 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-import { Platform } from 'react-native';
+const tintColorLight = "#0a7ea4";
+const tintColorDark = "#fff";
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
-
-export const Colors = {
-  light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
-  },
+const common = {
+	primary: "#266DD3",
+	"primary-01": "#E9F0FB",
+	"primary-content": "#fff",
+	shade: "#212121",
+	"shade-content": "#fff",
+	accent: "#F59E0B",
 };
 
-export const Fonts = Platform.select({
-  ios: {
-    /** iOS `UIFontDescriptorSystemDesignDefault` */
-    sans: 'system-ui',
-    /** iOS `UIFontDescriptorSystemDesignSerif` */
-    serif: 'ui-serif',
-    /** iOS `UIFontDescriptorSystemDesignRounded` */
-    rounded: 'ui-rounded',
-    /** iOS `UIFontDescriptorSystemDesignMonospaced` */
-    mono: 'ui-monospace',
-  },
-  default: {
-    sans: 'normal',
-    serif: 'serif',
-    rounded: 'normal',
-    mono: 'monospace',
-  },
-  web: {
-    sans: "system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif",
-    serif: "Georgia, 'Times New Roman', serif",
-    rounded: "'SF Pro Rounded', 'Hiragino Maru Gothic ProN', Meiryo, 'MS PGothic', sans-serif",
-    mono: "SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace",
-  },
-});
+export const Colors = {
+	light: {
+		...common,
+		text: "#11181C",
+		background: "#fff",
+		tint: tintColorLight,
+		icon: "#687076",
+		tabIconDefault: "#687076",
+		tabIconSelected: tintColorLight,
+	},
+	dark: {
+		...common,
+		text: "#f0f0f0",
+		background: "#0000",
+		tint: tintColorDark,
+		icon: "#9BA1A6",
+		tabIconDefault: "#9BA1A6",
+		tabIconSelected: tintColorDark,
+	},
+};
+
+export const Fonts = {
+	thin: "Inter_100",
+	extralight: "Inter_200",
+	light: "Inter_300",
+	regular: "Inter_400",
+	medium: "Inter_500",
+	semibold: "Inter_600",
+	bold: "Inter_700",
+	extrabold: "Inter_800",
+	black: "Inter_900",
+};
