4246548b676795e50aca5f42ad61fe5090ac25cc
Ezekiel Victor <codeepoch@gmail.com>
2026-06-20 13:20:14 +0100

    Trigger: build [ios] [android]

--- components/atoms/a-location-card.tsx
+++ components/atoms/a-location-card.tsx
@@ -1,11 +1,11 @@
-import { useThemeColors } from '@/lib/hooks/use-theme-color';
-import { Pressable, View } from 'react-native';
-import ExpoMap from './a-map';
-import React from 'react';
-import { openGoogleMaps } from '@/lib/utils/urls';
-import { MaterialSymbolsExpandContentRounded } from '../icons/i-fullscreen';
-import { useRouter } from '@/lib/hooks/use-router';
-import { ArrowBendUpRightBold } from '../icons/i-directions';
+import { useThemeColors } from "@/lib/hooks/use-theme-color";
+import { Pressable, View } from "react-native";
+import ExpoMap from "./a-map";
+import React from "react";
+import { openGoogleMaps } from "@/lib/utils/urls";
+import { MaterialSymbolsExpandContentRounded } from "../icons/i-fullscreen";
+import { useRouter } from "@/lib/hooks/use-router";
+import { ArrowBendUpRightBold } from "../icons/i-directions";
 
 type Props = {
   location?: {
@@ -32,8 +32,15 @@ const LocationCard: React.FC<Props> = ({
   const zoomLevel = React.useMemo(() => zoom ?? 6, [zoom]);
 
   return (
-    <View className="relative overflow-hidden rounded-xl border" style={{ height: 250 }}>
-      <ExpoMap title={title ?? undefined} coordinates={location} zoom={zoomLevel} />
+    <View
+      className="relative overflow-hidden rounded-xl border"
+      style={{ height: 250 }}
+    >
+      <ExpoMap
+        title={title ?? undefined}
+        coordinates={location}
+        zoom={zoomLevel}
+      />
       {withActions && (
         <View className="absolute right-2 top-2 flex-row items-center gap-4">
           {location && (
@@ -48,13 +55,16 @@ const LocationCard: React.FC<Props> = ({
           <Pressable
             onPress={() =>
               router.push(
-                `/hostings/map?latitude=${location?.latitude}&longitude=${location?.longitude}&zoom=${zoomLevel}${hostingId ? `&hostingId=${hostingId}&title=${encodeURIComponent(title ?? '')}&price=${price ?? 0}` : ''}`,
+                `/hostings/map?latitude=${location?.latitude}&longitude=${location?.longitude}&zoom=${zoomLevel}${hostingId ? `&hostingId=${hostingId}&title=${encodeURIComponent(title ?? "")}&price=${price ?? 0}` : ""}`,
               )
             }
             className="items-center justify-center rounded-2xl p-1 px-3"
             style={{ backgroundColor: colors.text }}
           >
-            <MaterialSymbolsExpandContentRounded size={24} color={colors.background} />
+            <MaterialSymbolsExpandContentRounded
+              size={24}
+              color={colors.background}
+            />
           </Pressable>
         </View>
       )}
